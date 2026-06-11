eaedd47966040b217fcf84d3ec93b8b71dbebd87
DDayDanthony <iq23qj@brocku.ca>
2026-06-11 19:09:03 -0400

    added specific links to my google sheets for image hosting.

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 // ===========================================
 
 const SHEET_URLS = {
-  original: "PASTE_ORIGINAL_ART_CSV_LINK_HERE",
-  fanart: "PASTE_FAN_ART_CSV_LINK_HERE",
-  commissions: "PASTE_COMMISSIONS_CSV_LINK_HERE",
-  blog: "PASTE_BLOG_CSV_LINK_HERE"
+  original: "https://docs.google.com/spreadsheets/d/1toERaUUQP4HSnd2gxyFu-aBJFwafB3VO0PnhbY7lHZ0/edit?gid=0#gid=0",
+  fanart: "https://docs.google.com/spreadsheets/d/1toERaUUQP4HSnd2gxyFu-aBJFwafB3VO0PnhbY7lHZ0/edit?gid=1758157628#gid=1758157628",
+  commissions: "https://docs.google.com/spreadsheets/d/1toERaUUQP4HSnd2gxyFu-aBJFwafB3VO0PnhbY7lHZ0/edit?gid=1962887599#gid=1962887599",
+  blog: "https://docs.google.com/spreadsheets/d/1toERaUUQP4HSnd2gxyFu-aBJFwafB3VO0PnhbY7lHZ0/edit?gid=626238044#gid=626238044"
 };
 
 // how many items show in the homepage carousel
